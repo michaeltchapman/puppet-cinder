831a1a429202c6420d48ef123bfea9284367dc4a
root <dotalton@cisco.com>
2013-02-20 12:55:11 -0500

    create cinder-volumes as a persistent lv

--- manifests/setup_test_volume.pp
+++ manifests/setup_test_volume.pp
@@ -1,11 +1,14 @@
 class cinder::setup_test_volume(
-  $volume_name     = 'cinder-volumes',
-  $size            = '4G',
-  $loopback_device = '/dev/loop2'
+  $volume_name     = 'cinder.loopback',
+  $volume_group    = 'cinder-volumes',
+  $size            = '1G',
+  $extents         = '255', #255 for 1G, 511 for 2G
+  $loopback_device = '/dev/loop0',
+  $mount_point     = '/cinder-volumes',
 ) {
 
   Exec {
-    cwd => '/tmp/',
+    cwd => '/var/tmp/',
   }
 
   exec { "/bin/dd if=/dev/zero of=${volume_name} bs=1 count=0 seek=${size}":
@@ -21,6 +24,15 @@ class cinder::setup_test_volume(
   } ~>
 
   exec { "/sbin/vgcreate ${volume_name} ${loopback_device}":
+    refreshonly => true,
+  } ~>
+  exec { "/sbin/lvcreate -l ${extents} -n ${volume_group} ${volume_name}":
+    unless => "/sbin/lvdisplay ${volume_group}"
+  } ~>
+  exec { "/sbin/mkfs.ext4 /dev/${volume_name}/${volume_group && mount /dev/${volume_name}/${volume_group ":
+    refreshonly => true, 
+  } ~>
+  exec { "/bin/cat \"/dev/cinder.loopback/cinder-volumes /cinder-volumes ext4 defaults 0 0\" >> /etc/fstab":
     refreshonly => true,
   }
 
