844b0ea307a903b3fc6e6be118e7b27b0e0e5a2b
root <dotalton@cisco.com>
2013-02-20 16:56:07 -0500

    rewrote setup_test_volume to create a persistent volume

--- manifests/setup_test_volume.pp
+++ manifests/setup_test_volume.pp
@@ -1,4 +1,5 @@
 class cinder::setup_test_volume(
+  $volume_file     = '/var/tmp/cinder.loopback',
   $volume_name     = 'cinder.loopback',
   $volume_group    = 'cinder-volumes',
   $size            = '1G',
@@ -7,34 +8,29 @@ class cinder::setup_test_volume(
   $mount_point     = '/cinder-volumes',
 ) {
 
-  Exec {
-    cwd => '/var/tmp/',
+  file { "/cinder-volumes":
+    ensure => directory,
+    owner  => 'root',
+  }
+  file_line { 'fstab_volume':
+    path => '/etc/fstab',
+    line => '/dev/cinder.loopback/cinder-volumes /cinder-volumes ext4 defaults 0 0',
   }
 
-  exec { "/bin/dd if=/dev/zero of=${volume_name} bs=1 count=0 seek=${size}":
-    unless => "/sbin/vgdisplay ${volume_name}"
+  exec { "/bin/dd if=/dev/zero of=${volume_file} bs=1 count=0 seek=${size}":
+    creates => "${volume_file}",
+    unless => "/sbin/vgdisplay ${volume_name}",
   } ~>
-
-  exec { "/sbin/losetup ${loopback_device} ${volume_name}":
-    refreshonly => true,
+  exec { "/sbin/losetup ${loopback_device} ${volume_file}":
+    subscribe   => File["/cinder-volumes"],
   } ~>
-
-  exec { "/sbin/pvcreate ${loopback_device}":
-    refreshonly => true,
-  } ~>
-
-  exec { "/sbin/vgcreate ${volume_name} ${loopback_device}":
-    refreshonly => true,
-  } ~>
+  exec { "/sbin/pvcreate ${loopback_device}": } ~>
+  exec { "/sbin/vgcreate ${volume_name} ${loopback_device}": } ~>
   exec { "/sbin/lvcreate -l ${extents} -n ${volume_group} ${volume_name}":
-    unless => "/sbin/lvdisplay ${volume_group}"
+    unless => "/sbin/lvdisplay ${volume_group}",
   } ~>
-  exec { "/sbin/mkfs.ext4 /dev/${volume_name}/${volume_group && mount /dev/${volume_name}/${volume_group ":
-    refreshonly => true, 
-  } ~>
-  exec { "/bin/cat \"/dev/cinder.loopback/cinder-volumes /cinder-volumes ext4 defaults 0 0\" >> /etc/fstab":
-    refreshonly => true,
+  exec { "/sbin/mkfs.ext4 /dev/${volume_name}/${volume_group}": } ~>
+  exec { "/bin/mount /dev/${volume_name}/${volume_group} ${mount_point}":
+    require     => File['/cinder-volumes'],
   }
-
 }
-
